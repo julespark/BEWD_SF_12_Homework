e95b2087dd42b038c10870decd9192b5364718a9
jstarr326 <jstarrtaylor@gmail.com>
2015-11-23 16:47:16 -0800

    made it 10 guesses w/ 1-100

--- JordanTaylor-jstarr326/hw2/hw1_secret.rb
+++ JordanTaylor-jstarr326/hw2/hw1_secret.rb
@@ -18,35 +18,33 @@ name = gets.chomp
 puts "Hi #{name}!"
 puts "Here are the rules. You must guess a number between 1 and 10. You will only have two tries!"
 
-secret = 4
+secret = rand(1..100)
 tries = 0
 puts "Go ahead, what is your guess?"
 guess = gets.chomp.to_i
 
 
-while guess != secret && tries == 0
+while guess != secret && tries <10
 
 	if guess > secret
 		tries += 1
-		puts "You guess too high, one more try!"
+		puts "You guess too high, try again!"
 		guess = gets.chomp.to_i
 
 	elsif guess < secret
 		tries += 1
-		puts "You guess too low, one more try!"
+		puts "You guess too low, try again!"
 		guess = gets.chomp.to_i
 	end
 end
 
-if guess != secret && tries == 1
+if guess != secret && tries == 10
 	puts "You ran out of guesses. The number was four"
 end
 
 if guess == secret
 	puts "you win!"
 end
-
-
 
 
 
