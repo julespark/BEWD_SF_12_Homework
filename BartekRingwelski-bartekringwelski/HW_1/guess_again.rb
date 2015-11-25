36c87eff68106581b3743776e3d1608edf428937
bartekringwelski <bartekr@gmail.com>
2015-11-24 17:18:35 -0800

    hw1

--- BartekRingwelski-bartekringwelski/HW_1/guess_again.rb
+++ BartekRingwelski-bartekringwelski/HW_1/guess_again.rb
@@ -6,33 +6,67 @@ player_name = gets.chomp.to_s
 
 puts "Hi, " + player_name + "!  You have 2 guesses to guess my secret number between 1 and 10. What is your 1st guess?"
 
-guess_1 = gets.chomp.to_i
 
 
-
-
-def guess_check guess_1
-	 secret_number = 5
+def guess_check 
+	secret_number = 5
+	guess_1 = gets.chomp.to_i
 
 
 	if guess_1 == secret_number
 		puts "you got it right"
 
-	else
-		puts "you got it wrong"
-
-	end
+		else 
+			puts "try again. Enter new guess"
+			guess_2 = gets.chomp.to_i
+				if guess_2 == secret_number
+					puts "you got it right the 2nd time"
+				else 
+					puts "still wrong :(. The secret number was " + secret_number.to_s + "."
+				end
+		end
 end
 
-guess_check (guess_1)
+guess_check
 
 
 
 
-# def say (something)
-#       puts "They said " + something.to_s
+
+# 	els
+# 		guess_1 > secret_number
+# 		puts "you got it wrong, guess lower"
+		
+# 			guess_2 = gets.chomp.to_i
+# 			if guess_2 == secret_number
+# 				puts "you got it right (finally, what took you so long?)" 
+# 			else puts "you got it wrong, mofo. Game over"
+# 	else
+# 		puts "you got it wrong, guess higher"
+# 			guess_2 = gets.chomp.to_i
+# 			if guess_2 == secret_number
+# 				puts "you got it right (finally, what took you so long?)" 
+# 			else puts "you got it wrong, mofo. Game over"
+# 	end
 # end
 
-#  say ("Hello")
 
-# => They said Hello
+
+
+# def guess_check_2 guess_2
+# 	 secret_number = 5
+# 	if guess_2 == secret_number
+# 		puts "you got it right (finally, what took you so long?)"
+
+# 	else
+# 		puts "you got it wrong, mofo. Game over"
+# 	end
+# end
+
+
+
+
+# if guess check 1 matches, end, ifelse, run guess check 2
+
+
+
