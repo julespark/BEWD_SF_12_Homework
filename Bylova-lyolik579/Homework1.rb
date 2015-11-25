d54eee2669f0859c3a8691b3b1d4ef4257e055ad
lyolik579 <oabyloav@gmail.com>
2015-11-25 01:47:04 +0000

    committing homework 1

---
+++ Bylova-lyolik579/Homework1.rb
@@ -0,0 +1,35 @@
+attempts = 2    
+
+    puts "We're gonna play a game! Please tell me your name"
+    player_name = gets
+    puts "Welcome #{player_name}"
+    puts "Guess a number between 1 - 10. You only have 2 attempts!"
+    player_input = gets.to_i
+    secret_number = 6
+
+
+    def guess_check ( player_input, secret_number )     
+        if player_input > secret_number
+            puts "Wrong, sorry! Too high!"
+        elsif player_input < secret_number
+            puts "Wrong, sorry! Too low!" 
+        else
+            puts "Congratulations, you've guessed the secret number! #{[secret_number]}"
+            attempts==0
+        end
+    end
+        
+
+    while attempts > 0
+        guess_check( player_input, secret_number )
+        attempts = attempts - 1
+        if attempts == 0
+            puts "Sorry, you have no more tries"
+        else
+            puts "Guess the secret number (You have #{attempts} tries left):"
+        end
+
+        player_input = gets.to_i
+
+    end
+
