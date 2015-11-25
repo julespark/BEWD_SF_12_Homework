1204c1265aa37d7b0588bb6532073004aafba753
bartekringwelski <bartekr@gmail.com>
2015-11-24 17:58:34 -0800

    new better way

--- BartekRingwelski-bartekringwelski/HW_1/guess_again.rb
+++ BartekRingwelski-bartekringwelski/HW_1/guess_again.rb
@@ -6,52 +6,64 @@ player_name = gets.chomp.to_s
 
 puts "Hi, " + player_name + "!  You have 2 guesses to guess my secret number between 1 and 10. What is your 1st guess?"
 
+secret_number = 5
 
 
-def guess_check 
-	secret_number = 5
+
+########################################################################################
+#THIS IS THE BETTER WAY TO DO THE HW BC IT IS EASIER TO READ/UNDERSTAND
+
+def guess_check (secret_number)
 	guess_1 = gets.chomp.to_i
 
-
-	if guess_1 == secret_number
+	case 
+	when guess_1 == secret_number
 		puts "you got it right"
 
-		elsif guess_1 > secret_number
-			puts "too high. Enter new guess"
-			guess_2 = gets.chomp.to_i
-				if guess_2 == secret_number
-					puts "you got it right the 2nd time"
-				else 
-					puts "still wrong :(. The secret number was " + secret_number.to_s + "."
-				end
-		else 
-			puts "too low. enter new guess"
-			guess_2 = gets.chomp.to_i
-				if guess_2 == secret_number
-					puts "you got it right the 2nd time"
-				else 
-					puts "still wrong :(. The secret number was " + secret_number.to_s + "."
-				end
-		end
+	when guess_1 > secret_number
+		puts "guess lower"
+		guess_check_2 (secret_number)
+	else
+		puts "guess higher" 
+		guess_check_2 (secret_number)
+	end
 end
 
-guess_check
+
+def guess_check_2 (secret_number)
+	guess_2 = gets.chomp.to_i
+
+	if guess_2 == secret_number
+		puts "phew...you got it right"
+
+	else 
+		puts "you failed at this game." 
+	end
+end
 
 
+guess_check (secret_number)
+
+################################################################################
 
 
-
-
-# def guess_check 
-# 	secret_number = 5
+# THIS IS THE LESS SOPHISTICATED WAY I DID THE HW, REQUIRING 25 LINES OF CODE
+# def guess_check (secret_number)
 # 	guess_1 = gets.chomp.to_i
-
 
 # 	if guess_1 == secret_number
 # 		puts "you got it right"
 
+# 		elsif guess_1 > secret_number
+# 			puts "too high. Enter new guess"
+# 			guess_2 = gets.chomp.to_i
+# 				if guess_2 == secret_number
+# 					puts "you got it right the 2nd time"
+# 				else 
+# 					puts "still wrong :(. The secret number was " + secret_number.to_s + "."
+# 				end
 # 		else 
-# 			puts "try again. Enter new guess"
+# 			puts "too low. enter new guess"
 # 			guess_2 = gets.chomp.to_i
 # 				if guess_2 == secret_number
 # 					puts "you got it right the 2nd time"
@@ -61,4 +73,8 @@ guess_check
 # 		end
 # end
 
-# guess_check
+# guess_check (secret_number)
+
+
+
+
