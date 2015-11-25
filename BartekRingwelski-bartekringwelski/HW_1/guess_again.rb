bca918cd4064bbe7704259ec42ba7a28f1a58b12
bartekringwelski <bartekr@gmail.com>
2015-11-24 17:26:01 -0800

    final hw1

--- BartekRingwelski-bartekringwelski/HW_1/guess_again.rb
+++ BartekRingwelski-bartekringwelski/HW_1/guess_again.rb
@@ -16,8 +16,16 @@ def guess_check
 	if guess_1 == secret_number
 		puts "you got it right"
 
+		elsif guess_1 > secret_number
+			puts "too high. Enter new guess"
+			guess_2 = gets.chomp.to_i
+				if guess_2 == secret_number
+					puts "you got it right the 2nd time"
+				else 
+					puts "still wrong :(. The secret number was " + secret_number.to_s + "."
+				end
 		else 
-			puts "try again. Enter new guess"
+			puts "too low. enter new guess"
 			guess_2 = gets.chomp.to_i
 				if guess_2 == secret_number
 					puts "you got it right the 2nd time"
@@ -33,40 +41,24 @@ guess_check
 
 
 
-# 	els
-# 		guess_1 > secret_number
-# 		puts "you got it wrong, guess lower"
-		
+
+# def guess_check 
+# 	secret_number = 5
+# 	guess_1 = gets.chomp.to_i
+
+
+# 	if guess_1 == secret_number
+# 		puts "you got it right"
+
+# 		else 
+# 			puts "try again. Enter new guess"
 # 			guess_2 = gets.chomp.to_i
-# 			if guess_2 == secret_number
-# 				puts "you got it right (finally, what took you so long?)" 
-# 			else puts "you got it wrong, mofo. Game over"
-# 	else
-# 		puts "you got it wrong, guess higher"
-# 			guess_2 = gets.chomp.to_i
-# 			if guess_2 == secret_number
-# 				puts "you got it right (finally, what took you so long?)" 
-# 			else puts "you got it wrong, mofo. Game over"
-# 	end
+# 				if guess_2 == secret_number
+# 					puts "you got it right the 2nd time"
+# 				else 
+# 					puts "still wrong :(. The secret number was " + secret_number.to_s + "."
+# 				end
+# 		end
 # end
 
-
-
-
-# def guess_check_2 guess_2
-# 	 secret_number = 5
-# 	if guess_2 == secret_number
-# 		puts "you got it right (finally, what took you so long?)"
-
-# 	else
-# 		puts "you got it wrong, mofo. Game over"
-# 	end
-# end
-
-
-
-
-# if guess check 1 matches, end, ifelse, run guess check 2
-
-
-
+# guess_check
