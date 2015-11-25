8100e12748cbf13f25915bf40b166633a92c4e33
lyolik579 <oabyloav@gmail.com>
2015-11-25 02:19:11 +0000

    submit hmw1

--- Bylova-lyolik579/Homework1.rb
+++ Bylova-lyolik579/Homework1.rb
@@ -8,14 +8,14 @@ attempts = 2
     secret_number = 6
 
 
-    def guess_check ( player_input, secret_number )     
-        if player_input > secret_number
-            puts "Wrong, sorry! Too high!"
-        elsif player_input < secret_number
-            puts "Wrong, sorry! Too low!" 
-        else
+    def guess_check ( player_input, secret_number )    
+        if player_name = secret_number
             puts "Congratulations, you've guessed the secret number! #{[secret_number]}"
-            attempts==0
+            exit
+        elsif player_input > secret_number
+            puts "Wrong, sorry! Too high!"
+        else player_input < secret_number
+            puts "Wrong, sorry! Too low!"
         end
     end
         
@@ -23,7 +23,7 @@ attempts = 2
     while attempts > 0
         guess_check( player_input, secret_number )
         attempts = attempts - 1
-        if attempts == 0
+        if attempts <= 0
             puts "Sorry, you have no more tries"
         else
             puts "Guess the secret number (You have #{attempts} tries left):"
