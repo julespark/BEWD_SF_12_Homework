da1be534bbed78d3d83aad8a67226955673ee3d3
Olie <oabylova@gmail.com>
2016-01-19 17:23:40 -0800

    Twitter app

--- Bylova-lyolik579/Homework8/TwitterApp/TwitterApp/db/seeds.rb
+++ Bylova-lyolik579/Homework8/TwitterApp/TwitterApp/db/seeds.rb
@@ -1,4 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+User.create
+  {
+    name: "Fluffy cat",
+    premium_member: "0"
+  },
+ Tweet.create
+  {
+    text: "I'm so fluffy"
+  },
+Hashtag.create
+  {
+    name: "cats"
+  }
+
+
+
+
+  # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
